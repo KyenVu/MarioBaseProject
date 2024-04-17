11a8eae3f9455e15a9d02d956391785502188870
KyenVu <KyenVu264@gmail.com>
2024-04-17 11:22:10 +0700

    Finish rendering image to window
    17/04

--- MarioBaseProject/Source.cpp
+++ MarioBaseProject/Source.cpp
@@ -11,12 +11,81 @@ SDL_Window* g_window = nullptr;
 SDL_Renderer* g_renderer = nullptr;
 SDL_Texture* g_texture = nullptr;
 
-void Render();
-SDL_Texture* LoadTextureFromFile(string path);
-void FreeTexture();
-
 bool InitSDL();
 void CloseSDL();
+bool Update();
+void Render();
+SDL_Texture* LoadTextureFromFile(string path);
+
+bool InitSDL()
+{
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        cout << "SDL did not initialise. Error: " << SDL_GetError();
+        return false;
+    }
+
+    // Create window
+    g_window = SDL_CreateWindow("Games Engine Creation",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        SDL_WINDOW_RESIZABLE);
+
+    if (g_window == nullptr)
+    {
+        cout << "Window was not created. Error : " << SDL_GetError();
+        return false;
+    }
+
+    // Create renderer
+    g_renderer = SDL_CreateRenderer(g_window, -1, SDL_RENDERER_ACCELERATED);
+
+    if (g_renderer == nullptr)
+    {
+        cout << "Renderer could not be created. Error: " << SDL_GetError();
+        return false;
+    }
+
+    // Initialize PNG loading
+    int imageFlags = IMG_INIT_PNG;
+    if (!(IMG_Init(imageFlags) & imageFlags))
+    {
+        cout << "SDL_Image could not initialize. Error: " << IMG_GetError();
+        return false;
+    }
+
+    // Load the texture
+    g_texture = LoadTextureFromFile("Images/garnacho.bmp");
+    if (g_texture == nullptr)
+    {
+        cout << "Failed to load texture.";
+        return false;
+    }
+
+    return true;
+}
+
+void CloseSDL()
+{
+    // Release the texture
+    SDL_DestroyTexture(g_texture);
+    g_texture = nullptr;
+
+    // Destroy renderer
+    SDL_DestroyRenderer(g_renderer);
+    g_renderer = nullptr;
+
+    // Destroy window
+    SDL_DestroyWindow(g_window);
+    g_window = nullptr;
+
+    // Quit SDL subsystems
+    IMG_Quit();
+    SDL_Quit();
+}
 
 bool Update()
 {
@@ -38,6 +107,49 @@ bool Update()
     return false;
 }
 
+void Render()
+{
+    // Clear the screen
+    SDL_SetRenderDrawColor(g_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_RenderClear(g_renderer);
+
+    // Set where to render the texture
+    SDL_Rect renderLocation = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
+
+    // Render to screen
+    SDL_RenderCopy(g_renderer, g_texture, NULL, &renderLocation);
+
+    // Update the screen
+    SDL_RenderPresent(g_renderer);
+}
+
+SDL_Texture* LoadTextureFromFile(string path)
+{
+    // The final texture
+    SDL_Texture* newTexture = nullptr;
+
+    // Load image at specified path
+    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+    if (loadedSurface == nullptr)
+    {
+        cout << "Unable to load image " << path << ". SDL_image Error: " << IMG_GetError();
+    }
+    else
+    {
+        // Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface(g_renderer, loadedSurface);
+        if (newTexture == nullptr)
+        {
+            cout << "Unable to create texture from " << path << ". SDL Error: " << SDL_GetError();
+        }
+
+        // Get rid of old loaded surface
+        SDL_FreeSurface(loadedSurface);
+    }
+
+    return newTexture;
+}
+
 int main(int argc, char* args[])
 {
     // Flag to check if we wish to quit
@@ -52,94 +164,12 @@ int main(int argc, char* args[])
     // Game Loop
     while (!quit)
     {
-        quit = Update();
+        Render(); // Render texture to screen
+        quit = Update(); // Check for quit event
     }
 
     // Close SDL before ending the program
     CloseSDL();
+
     return 0;
-}
-
-// Function prototypes
-bool InitSDL()
-{
-    // Set up SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        cout << "SDL did not initialise. Error: " << SDL_GetError();
-        return false;
-    }
-    else
-    {
-        // set up passed so create window
-        g_window = SDL_CreateWindow("Games Engine Creation",
-            SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED,
-            SCREEN_WIDTH,
-            SCREEN_HEIGHT,
-            SDL_WINDOW_SHOWN);
-        // did the window get created ?
-        if (g_window == nullptr)
-        {
-            // window failed
-            cout << "Window was not created. Error : " << SDL_GetError();
-            return false;
-        }
-    }
-    g_renderer = SDL_CreateRenderer(g_window, -1, SDL_RENDERER_ACCELERATED);
-
-    if (g_renderer != nullptr)
-    {
-        //init PNG loading
-        int imageFlags = IMG_INIT_PNG;
-        if (!(IMG_Init(imageFlags) & imageFlags))
-        {
-            cout << "SDL_Image could not initialize. Error: " << IMG_GetError();
-            return false;
-        }
-    }
-    else
-    {
-        cout << "Render could not initialize. Error: " << IMG_GetError();
-        return false;
-    }
-
-    if (g_renderer != nullptr)
-    {
-        //init PNG loading
-        int imageFlags = IMG_INIT_PNG;
-        if (!(IMG_Init(imageFlags) & imageFlags))
-        {
-            cout << "SDL_Image could not initialize. Error: " << IMG_GetError();
-            return false;
-
-        }
-    }
-
-    return true; // Return true if SDL initialization is successful
-}
-
-void Render()
-{
-    //Clear the screen
-    SDL_SetRenderDrawColor(g_renderer, 0xff, 0xff, 0xff, 0xff);
-    SDL_RenderClear(g_renderer);
-
-    //Set where to render the texture
-    SDL_Rect renderLocation = { 0,0, SCREEN_HEIGHT, SCREEN_WIDTH };
-
-    //Render to screen
-    SDL_RenderCopyEx(g_renderer, g_texture, NULL, &renderLocation, 0, NULL, SDL_FLIP_NONE);
-
-    
-}
-
-void CloseSDL()
-{
-    // release the window
-    SDL_DestroyWindow(g_window);
-    g_window = nullptr;
-    // quit SDL subsystems
-    IMG_Quit();
-    SDL_Quit();
 }
